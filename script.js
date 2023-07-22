eb393e7b38a1dec93fd814f6d397d182f8dbda45
BodruddozaRedoy <bodruddozaredoy@gmail.com>
2023-07-22 23:45:31 +0600

    ghg

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
+// loginBtn Event handlar 
 const loginBtn = document.getElementById("login")
 loginBtn.addEventListener("click", function(){
     const loginArea = document.getElementById("login-area").style.display = "none"
     const transactionArea = document.getElementById("transaction-area").style.display = "block"
+})
+
+// Deposit button event 
+const depositBtn = document.getElementById("deposit").addEventListener("click", function(){
+    console.log("jksdhfkjsdfh")
 })
