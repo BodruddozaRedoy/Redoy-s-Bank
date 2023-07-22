3a89cd729147acad47863b577d92f0c15fc7b195
BodruddozaRedoy <bodruddozaredoy@gmail.com>
2023-07-22 23:39:50 +0600

    hh

---
+++ script.js
@@ -0,0 +1,5 @@
+const loginBtn = document.getElementById("login")
+loginBtn.addEventListener("click", function(){
+    const loginArea = document.getElementById("login-area").style.display = "none"
+    const transactionArea = document.getElementById("transaction-area").style.display = "block"
+})
