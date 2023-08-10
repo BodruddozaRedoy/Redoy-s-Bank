2752d5a1c1920aa195aabec0ae30bcd97639bc85
BodruddozaRedoy <bodruddozaredoy@gmail.com>
2023-08-10 21:47:56 +0600

    done

--- script.js
+++ script.js
@@ -6,6 +6,41 @@ loginBtn.addEventListener("click", function(){
 })
 
 // Deposit button event 
-const depositBtn = document.getElementById("deposit").addEventListener("click", function(){
-    console.log("jksdhfkjsdfh")
+    const depositBtn = document.getElementById("addDeposit").addEventListener("click", function(){
+    const depositAmount = document.getElementById("depositAmount").value;
+    const depositNum = parseFloat(depositAmount)
+
+    const currentDeposit = document.getElementById("currentDeposit").innerText;
+    const currentDepositNum = parseFloat(currentDeposit);
+    const totalDeposit = depositNum + currentDepositNum;
+    document.getElementById("currentDeposit").innerText = totalDeposit;
+
+    const currentBalance = document.getElementById("currentBalance").innerText
+    const currentBalanceNum = parseFloat(currentBalance)
+    const totalBalance = depositNum + currentBalanceNum;
+    document.getElementById("currentBalance").innerText = totalBalance;
+
+    document.getElementById("depositAmount").value = ""
+    })
+// Withdraw button event 
+
+    const withdrawBtn = document.getElementById("addwithdraw").addEventListener("click", function(){
+    const withdrawAmount = document.getElementById("withdrawAmount").value;
+    const withdrawNum = parseFloat(withdrawAmount)
+
+    const currentWithdraw = document.getElementById("currentWithdraw").innerText;
+    const currentWithdrawNum = parseFloat(currentWithdraw);
+    const totalWithdraw = withdrawNum + currentWithdrawNum;
+    document.getElementById("currentWithdraw").innerText = totalWithdraw;
+
+    const currentBalance = document.getElementById("currentBalance").innerText
+    const currentBalanceNum = parseFloat(currentBalance)
+    const totalBalance = currentBalanceNum - withdrawNum;
+    document.getElementById("currentBalance").innerText = totalBalance;
+
+    document.getElementById("withdrawAmount").value = ""
+    
+
+    
+
 })
